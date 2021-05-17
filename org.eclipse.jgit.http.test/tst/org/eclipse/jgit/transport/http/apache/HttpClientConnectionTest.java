37a1174032ddae04612e04a1817b7f6e9f4682f5
15301062826 <57606708+15301062826@users.noreply.github.com>
2021-05-17 16:22:37 +0800

    Update HttpClientConnectionTest.java

--- org.eclipse.jgit.http.test/tst/org/eclipse/jgit/transport/http/apache/HttpClientConnectionTest.java
+++ org.eclipse.jgit.http.test/tst/org/eclipse/jgit/transport/http/apache/HttpClientConnectionTest.java
@@ -1,11 +1,5 @@
 /*
  * Copyright (C) 2018 Gabriel Couto <gmcouto@gmail.com>
- * and other copyright owners as documented in the project's IP log.
- *
- * This program and the accompanying materials are made available
- * under the terms of the Eclipse Distribution License v1.0 which
- * accompanies this distribution, is reproduced below, and is
- * available at http://www.eclipse.org/org/documents/edl-v10.php
  *
  * All rights reserved.
  *
